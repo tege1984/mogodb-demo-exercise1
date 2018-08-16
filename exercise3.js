ab9ee7c5f521185f1bfb2faf5e6087aed95286a5
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-08-16 09:37:53 +0300

    exercise3 completed

---
+++ exercise3.js
@@ -0,0 +1,41 @@
+// import mongoose library
+const mongoose = require("mongoose");
+
+// connect to the database
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to mongo-exercises db..."))
+  .catch(error =>
+    console.error("Could not connect to mongo-exercises db", error)
+  );
+
+// create schema
+const courseSchema = new mongoose.Schema({
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  name: String,
+  author: String,
+  isPublished: Boolean,
+  price: Number,
+  __v: Number
+});
+
+// creat a model
+const Course = mongoose.model("Course", courseSchema);
+
+// get courses
+async function getCourses() {
+  return await Course.find({
+    isPublished: true
+  })
+    .or([{ price: { $gte: 15 } }, { name: /.*by.*/i }])
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1, price: 1 });
+}
+// display courses
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();
